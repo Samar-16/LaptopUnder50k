703d74bfe7ca5527c1172420906474576abe94d2
Samar-16 <9694952835k@gmail.com>
2021-07-02 17:31:13 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {BrowserRouter as Router, Switch , Route}  from 'react-router-dom';
-import HomePage from './Components/HomaPage/HomePage'
+import HomePage from './Components/HomePage'
 import './App.css';
 function App() {
   return (
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import './HomePage.css'
 import Nav from './Navbar.js'
-import Footer from './Footer.js'
 import Content from './Content.js'
 export default class HomePage extends Component {
     render() {
